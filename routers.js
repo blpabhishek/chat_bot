ccd4a74c264743495049bcdc9b00ed4a17159bc2
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 19:22:44 +0530

    Changed

--- routers.js
+++ routers.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var bodyParser = require('./bodyParser.js');
+var request = require('request');
 var queryString = require('querystring');
-var cookie_parser = require('cookie-parser');
 var app = express();
 
 app.use(bodyParser);
-app.use(cookie_parser());
 
 const PAGE_TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
 
@@ -17,7 +16,7 @@ function sendTextMessage(sender, text) {
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
+    qs: {access_token:PAGE_TOKEN},
     method: 'POST',
     json: {
       recipient: {id:sender},
