c6ceec23b6b2f12d59522571ee3f6b620e3f65eb
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 19:16:37 +0530

    Changed

--- routers.js
+++ routers.js
@@ -7,7 +7,31 @@ var app = express();
 app.use(bodyParser);
 app.use(cookie_parser());
 
-const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
+const PAGE_TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
+
+
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+};
+
 
 app.get('/webhook/', function(req, res) {
     console.log('This is the query', req.query);
@@ -18,10 +42,7 @@ app.get('/webhook/', function(req, res) {
 
 
 app.post('/webhook', function(req, res) {
-	console.log('This is the final  data',req.body);
-
   var data = JSON.parse(Object.keys(req.body)[0]);
-  console.log('This is',data);
   var messaging_events = data.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = data.entry[0].messaging[i];
@@ -29,7 +50,7 @@ app.post('/webhook', function(req, res) {
     if (event.message && event.message.text) {
       text = event.message.text;
       console.log("Message",text);
-      // Handle a text message from this sender
+      sendTextMessage(sender,'I am Bot');
     }
   }
   res.sendStatus(200);
