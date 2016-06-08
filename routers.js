e6326d2231a344aef9744a268a1d2b8d23d11572
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 17:57:52 +0530

    Body came

--- routers.js
+++ routers.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var queryString = require('querystring');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-//app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
@@ -15,15 +16,7 @@ app.get('/webhook/', function(req, res){
 });
 
 
-app.post('/webhook/', function (req, res) {
-	var data = '';
-	req.on('data',function(chunk){
-		data +=  chunk;
-	});
-	req.on('end',function(){
-		data = queryString.parse(data);
-		req.body=data;
-	});
+app.post('/webhook', function (req, res) {
   console.log('This is the Body',req.body);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
