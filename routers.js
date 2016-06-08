d47dda78edf7df54daf21e38e43e360f5501bd47
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 17:40:22 +0530

    Changed the Token

--- routers.js
+++ routers.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-app.use(bodyParser.json({ type: 'application/*+json' }));
+//app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
@@ -16,10 +16,14 @@ app.get('/webhook/', function(req, res){
 
 
 app.post('/webhook/', function (req, res) {
-	var data ='';
+	var data = '';
 	req.on('data',function(chunk){
-		data+=chunk;
-	}).on;
+		data +=  chunk;
+	});
+	req.on('end',function(){
+		data = queryString.parse(data);
+		req.body=data;
+	});
   console.log('This is the Body',req.body);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
