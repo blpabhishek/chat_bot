02d9c1fe050832bdd90443baa51b900b4253f17c
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 18:57:25 +0530

    Added method

--- routers.js
+++ routers.js
@@ -1,11 +1,10 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var queryString = require('querystring');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// app.use(urlencodedParser)
+app.use(bodyParser);
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
@@ -19,26 +18,28 @@ app.get('/webhook/', function(req, res) {
 
 
 app.post('/webhook', function(req, res) {
-    var data = '';
-    req.on('data', function(chunk) {
-    	console.log('chunk',data);
-        data += chunk;
-    });
-    req.on('end', function() {
-        data = queryString.parse(data);
-        req.body = data;
-        console.log('This is the final  data',data);
-        messaging_events = req.body.entry[0].messaging;
-        for (i = 0; i < messaging_events.length; i++) {
-            event = req.body.entry[0].messaging[i];
-            sender = event.sender.id;
-            if (event.message && event.message.text) {
-                text = event.message.text;
-                console.log(text);
-            }
-        }
-        res.sendStatus(200);
-    });
+	console.log('This is the final  data',req.body);
+	res.sendStatus(200);
+    // var data = '';
+    // req.on('data', function(chunk) {
+    // 	console.log('chunk',data);
+    //     data += chunk;
+    // });
+    // req.on('end', function() {
+    //     data = queryString.parse(data);
+    //     req.body = data;
+    //     console.log('This is the final  data',data);
+    //     messaging_events = req.body.entry[0].messaging;
+    //     for (i = 0; i < messaging_events.length; i++) {
+    //         event = req.body.entry[0].messaging[i];
+    //         sender = event.sender.id;
+    //         if (event.message && event.message.text) {
+    //             text = event.message.text;
+    //             console.log(text);
+    //         }
+    //     }
+    //     res.sendStatus(200);
+    // });
 });
 
 module.exports = app;
