944169e3129ca5b2c24dd43299f1b66125f01a04
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 17:29:02 +0530

    Added a parser

--- routers.js
+++ routers.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
