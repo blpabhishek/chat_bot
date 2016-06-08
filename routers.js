46e46d46c9470e191c7a895d1a3b1451e72cda76
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 18:04:53 +0530

    Adding log

--- routers.js
+++ routers.js
@@ -4,12 +4,14 @@ var queryString = require('querystring');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-app.use(bodyParser.json({ type: 'application/*+json' }));
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser)
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
 
 app.get('/webhook/', function(req, res){
+	console.log(req.query);
     if (req.query['hub.verify_token'] === TOKEN)
         res.send(req.query['hub.challenge']);
     res.send('Error, wrong validation token');
