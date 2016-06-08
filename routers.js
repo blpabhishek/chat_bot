fecb1c6268591c86b0d32806fecac75c64efe1b0
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 17:35:01 +0530

    Changed the Token

--- routers.js
+++ routers.js
@@ -3,19 +3,23 @@ var bodyParser = require('body-parser');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
 
 app.get('/webhook/', function(req, res){
-    if (req.query['hub.verify_token'] === TOKEN)
+    if (req.query['hub.verify_token'] === 'TOKEN_FACEBOOK_BOTS')
         res.send(req.query['hub.challenge']);
     res.send('Error, wrong validation token');
 });
 
 
 app.post('/webhook/', function (req, res) {
+	var data ='';
+	req.on('data',function(chunk){
+		data+=chunk;
+	}).on;
   console.log('This is the Body',req.body);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
