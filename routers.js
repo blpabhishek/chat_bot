34f07f0a5e2842828ae2d6dbdd0c5ce65b92de74
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 18:40:40 +0530

    Changed the PArser

--- routers.js
+++ routers.js
@@ -10,36 +10,33 @@ app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
 
-app.get('/webhook/', function(req, res){
-	console.log('This is the query',req.query);
+app.get('/webhook/', function(req, res) {
+    console.log('This is the query', req.query);
     if (req.query['hub.verify_token'] === 'this_is_the_token')
         res.send(req.query['hub.challenge']);
     res.send('Error, wrong validation token');
 });
 
 
-app.post('/webhook', function (req, res) {
-	var data = '';
-	req.on('data',function(chunk){
-		data +=  chunk;
-		console.log(data);
-	});
-	req.on('end',function(){
-		data = queryString.parse(data);
-		req.body=data;
-		console.log(data);
-	});
-  console.log('This is the Body',req.body);
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log(text);
-    }
-  }
-  res.sendStatus(200);
+app.post('/webhook', function(req, res) {
+    var data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', function() {
+        data = queryString.parse(data);
+        req.body = data;
+        messaging_events = req.body.entry[0].messaging;
+        for (i = 0; i < messaging_events.length; i++) {
+            event = req.body.entry[0].messaging[i];
+            sender = event.sender.id;
+            if (event.message && event.message.text) {
+                text = event.message.text;
+                console.log(text);
+            }
+        }
+        res.sendStatus(200);
+    });
 });
 
 module.exports = app;
