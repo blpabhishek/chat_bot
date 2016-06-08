4ed938ac6c0ae68fc1764ecd1809b9791a40a57a
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 18:30:53 +0530

    Added events

--- routers.js
+++ routers.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 var queryString = require('querystring');
 var cookie_parser = require('cookie-parser');
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(urlencodedParser)
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// app.use(urlencodedParser)
 app.use(cookie_parser());
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
@@ -19,6 +19,16 @@ app.get('/webhook/', function(req, res){
 
 
 app.post('/webhook', function (req, res) {
+	var data = '';
+	req.on('data',function(chunk){
+		data +=  chunk;
+		console.log(data);
+	});
+	req.on('end',function(){
+		data = queryString.parse(data);
+		req.body=data;
+		console.log(data);
+	});
   console.log('This is the Body',req.body);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
