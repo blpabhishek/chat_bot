e30db78953ce055292b42b34c13e2267d2af681e
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-08 16:52:31 +0530

    Removed the http module.

--- routers.js
+++ routers.js
@@ -4,8 +4,10 @@ var app = express();
 
 const TOKEN = "EAAONmOIGzw4BAI4ZBY1u1CkTlVJlxWY66iI9e4ZAXJ0pTuZCfjXWkCCZBVQyke4fzxFMKHdCZBxtdwMSy1gWyPqkIcvt6Y2ngpQPztxEBm1yekcKZCrzKX4WVnZC1CZAob7QyU5Q89SPV3Er6uozmOI5NFdJRMugvVI4MwzdfWvhiwZDZD";
 
-app.get('/webhook/', function(req, res) {
+app.get('/webhook/', function(req, res){
     if (req.query['hub.verify_token'] === TOKEN)
         res.send(req.query['hub.challenge']);
     res.send('Error, wrong validation token');
 });
+
+module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var routers = require('./routers.js');
 const PORT = 5000;
 
-http.createServer(controller).listen(process.env.PORT || PORT,function(){
+routers.listen(process.env.PORT || PORT,function(){
 	console.log("listening at port===>"+ PORT);
 });
