106dc7b59c18a4425e4363e153460d9a63126260
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-11 17:46:07 +0530

    Adding WIT

--- libs/intentParser.js
+++ libs/intentParser.js
@@ -1,8 +1,23 @@
+var request = require('request');
 var intentParser = {};
 
+const MY_WIT_TOKEN = process.env.MY_WIT_TOKEN;
 
 intentParser.parseMessage = function(text){
-    return "Welcome, How can I help You";   
+request({
+    url: 'https://api.wit.ai/message',
+    data: {
+        'q': text,
+        'access_token' : MY_WIT_TOKEN
+    },
+    dataType: 'jsonp',
+    method: 'GET',
+    success: function(response) {
+      console.log(response);
+    }
+});
+    return "Welcome Here";
 }
+
 
 module.exports = intentParser;
